bdfd6d775ed39a0cebb16e3e0ab68bd91b5bad08
erikaosgue <1476@holbertonschool.com>
2020-04-28 02:02:48 +0000

    checks if a singly linked list has a cycle in it.

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a list has a cicle or loop in it
+ * @list: the list of nodes
+ * Return: 1 if there is a cycle, 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	while (list->next)
+	{
+		if ((list - (list->next)) < 0)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
