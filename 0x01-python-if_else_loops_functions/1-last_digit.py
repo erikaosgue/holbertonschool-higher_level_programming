c25f4a7e5e5a894fad2abc64dc15619b29b0a71c
erikaosgue <1476@holbertonschool.com>
2020-04-28 17:37:57 +0000

    Fix PEP8 problems

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 lastdigit = 0
 flag = 0
-
+str1 = ""
+str2 = ""
+str1 = "and is greater than 5"
+str2 = "and is less than 6 and not 0"
 if number < -9:
         number = number * -1
         flag = 1
@@ -15,10 +18,8 @@ if flag:
         number = number * -1
         lastdigit = lastdigit * -1
 if lastdigit > 5:
-        print("Last digit of", number, "is", lastdigit, "and is greater than 5")
-
+        print("Last digit of", number, "is", lastdigit, str1)
 elif lastdigit < 6 and lastdigit != 0:
-        print("Last digit of", number, "is", lastdigit, "and is less than 6 and not 0")
-
+        print("Last digit of", number, "is", lastdigit, str2)
 else:
         print("Last digit of", number, "is", lastdigit, "and is 0")
