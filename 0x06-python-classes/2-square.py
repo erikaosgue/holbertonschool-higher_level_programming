d2783cf820b76bcd4ff83e63d1a76549d5b29eaa
erikaosgue <1476@holbertonschool.com>
2020-05-19 22:39:06 +0000

    Class validates if the number is int and positive

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 2. Size validation"""
+
+
+class Square:
+    """A class Square that defines a square by:
+    Private instance attribute: size"""
+
+    __size = 0
+
+    def __init__(self, size=0):
+        """__init__ method to initialize an Instance
+        Args:
+            size (int): Number to be square
+        """
+        self.__size = size
+        if isinstance(self.__size, int):
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
