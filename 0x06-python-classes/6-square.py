d0bba528b791c2564969c12a77677236dd5e573b
erikaosgue <1476@holbertonschool.com>
2020-05-20 05:36:42 +0000

    Add else for conditional

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,8 +70,8 @@ class Square:
         """
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) or
-                not all(num >= 0 for num in value)):
+                not all(isinstance(number, int) for number in value) or
+                not all(number >= 0 for number in value)):
             raise TypeError('position must be a tuple of 2 positive integers')
         else:
             self.__position = value
