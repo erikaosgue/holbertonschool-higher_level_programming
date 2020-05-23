8bdff190909ba2ccc903706eb99e197a713410a7
erikaosgue <1476@holbertonschool.com>
2020-05-23 05:48:09 +0000

    Pep8

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""3. Print square a stringa function that prints a square
-with the character #.
-
-size is the size length of the square
-if size is a float and is less than 0,
- raise a TypeError exception with the message size must be an integer"""
-
+"""
+    3. Print square
+    print_suquare: a stringa function that prints a square
+    Return: nothing
+"""
 
 def print_square(size):
-    """Print square by the size
-    size
+    """
+        print square by the size
+        size
     """
 
     if isinstance(size, bool):
