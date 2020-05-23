c219d2ff1fa5bee9a17edfffa4b95ee501c55532
erikaosgue <1476@holbertonschool.com>
2020-05-23 01:57:54 +0000

    Add 2 matrix

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" 1. Divide a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """ Matriz of integers divided with div"""
+
+    msj = 'matrix must be a matrix (list of lists) of integers/floats'
+
+    if not isinstance(matrix, list) or isinstance(matrix, bool):
+        raise TypeError("1", msj)
+
+    if len(matrix) == 0 or not isinstance(matrix[0], list):
+        raise TypeError(msj)
+
+    if not matrix[0]:
+        raise TypeError(msj)
+
+    row_len = len(matrix[0])
+    for row in matrix:
+        if not isinstance(row, list) or len(row) != row_len:
+            raise TypeError('Each row of the matrix must have the same size')
+
+    for row in matrix:
+        for number in row:
+            if not isinstance(number, (int, float))\
+                    or isinstance(number, bool):
+                raise TypeError(msj)
+
+    if not isinstance(div, (int, float)) or isinstance(div, bool):
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(round((num / div), 2))
+        new_matrix.append(new_row)
+    return (new_matrix)
