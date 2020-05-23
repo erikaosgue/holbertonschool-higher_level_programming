b877735523717ab9b34359841afd62a5e69dd53a
erikaosgue <1476@holbertonschool.com>
2020-05-23 03:15:21 +0000

    Fix pep8 for condition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-""" 0. Integers addition"""
+""" 0. Integers addition
+Function that adds 2 integers.
+a and b must be integers or floats,
+raise a TypeError exception with the a message
+a and b must be first casted to integers if they are float"""
 
 
 def add_integer(a, b=98):
-    """Addition of two integers"""
+    """Addition of two integers
+    Args: a int b an int
+    """
+
     if not isinstance(a, (float, int)) or isinstance(a, bool):
         raise TypeError('a must be an integer')
 
