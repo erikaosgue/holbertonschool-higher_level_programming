9485d57a761e39df6b34a7cef4eed4204d32ca3d
erikaosgue <1476@holbertonschool.com>
2020-05-23 05:21:05 +0000

    Fix round int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """ 0. Integers addition
 Function that adds 2 integers.
-a and b must be integers or floats,
-raise a TypeError exception with the a message
 a and b must be first casted to integers if they are float"""
 
 
@@ -16,7 +14,7 @@ def add_integer(a, b=98):
 
     elif not isinstance(b, (float, int)) or isinstance(b, bool):
         raise TypeError('b must be an integer')
-    a = int(round(a))
-    b = int(round(b))
-    
+    else:
+        a = int(round(a))
+        b = int(round(b))
     return a + b
