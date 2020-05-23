94014b9d9b43eacce983730af400edf615e3f3f8
erikaosgue <1476@holbertonschool.com>
2020-05-23 03:24:54 +0000

    Fix pep8

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""2. Say my name"""
+"""2. Say my name  a function that prints a square with the character #.
+
+size is the size length of the square
+if size is a float and is less than 0,
+ raise a TypeError exception with the message size must be an integer"""
 
 
 def say_my_name(first_name, last_name=""):
-    """ Say my name"""
+    """ Say my name
+    Args first name, Last Name
+    """
 
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
