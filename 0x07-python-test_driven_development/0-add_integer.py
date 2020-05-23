eac4c5f25db305468f8e71cd482eb14f521ac7b7
erikaosgue <1476@holbertonschool.com>
2020-05-23 06:35:41 +0000

    Fix description

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-""" 0. Integers addition
-Function that adds 2 integers.
-a and b must be first casted to integers if they are float"""
+"""
+    0. Integers addition
+    add_integer: that adds 2 integers
+    Return: the sum
+"""
 
 
 def add_integer(a, b=98):
-    """Addition of two integers
-    Args: a int b an int
+    """
+        Addition of two integers
+        a: int
+        b an int
     """
 
     if not isinstance(a, (int, float)) or isinstance(a, bool):
-        raise TypeError('a must be an integer')
+        raise TypeError("a must be an integer")
 
     elif not isinstance(b, (int, float)) or isinstance(b, bool):
-        raise TypeError('b must be an integer')
+        raise TypeError("b must be an integer")
     else:
         a = int(round(a))
         b = int(round(b))
