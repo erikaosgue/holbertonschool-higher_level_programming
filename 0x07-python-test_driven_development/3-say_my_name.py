2e79d8568d0634aa83b7e53cadda79a538f5f83a
erikaosgue <1476@holbertonschool.com>
2020-05-24 02:49:09 +0000

    Add condition when last name is empty

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,7 +13,7 @@ def say_my_name(first_name, last_name=""):
     last_Name: The last name
     """
 
-    if not isinstance(first_name, str):
+    if not isinstance(first_name, str) or first_name == "":
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
         raise TypeError('last_name must be a string')
