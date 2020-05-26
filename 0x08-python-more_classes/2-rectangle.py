ddfc4b41d2c3d534ca59331a630e65ddeb440508
erikaosgue <1476@holbertonschool.com>
2020-05-26 00:17:44 +0000

    Add message that prints bye recatangle...

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -58,6 +58,6 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        if self.height == 0 or self.width == 0
+        if self.height == 0 or self.width == 0:
             return 0
         return (2 * self.height) + (2 * self.width)
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,7 +59,7 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        if self.height == 0 or self.width == 0
+        if self.height == 0 or self.width == 0:
             return 0
         return (2 * self.height) + (2 * self.width)
 
