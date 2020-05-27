cd028616f6ab029e904db7e6bd03acbc906f1902
erikaosgue <1476@holbertonschool.com>
2020-05-27 01:34:33 +0000

    Add the First version

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" 
+"""
     31. Low memory cost
     class LockedClass with no class or object attribute,
 """
+
+
 class LockedClass:
-    """ 
+    """
         creating new instance attributes
     """
     __slots__ = ["first_name"]
