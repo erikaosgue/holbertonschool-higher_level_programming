f13524d25a2426f1ed43b9538cf5e977833ba083
erikaosgue <1476@holbertonschool.com>
2020-06-02 23:08:48 +0000

    Add first version

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    13. Can I?
+    modulo: 101-add_attribute.py
+"""
+
+
+def add_attribute(obj, attribute, name):
+    """
+        function that adds a new attribute to an object
+    """
+    if str(type(obj)) != "<class '__main__.MyClass'>":
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, name)
