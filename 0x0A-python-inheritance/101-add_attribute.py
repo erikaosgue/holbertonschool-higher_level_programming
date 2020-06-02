6fa21787d69209f0188bca171b39082d2741b6b3
erikaosgue <1476@holbertonschool.com>
2020-06-02 23:18:29 +0000

    Add first version

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -11,7 +11,7 @@ def add_attribute(obj="", attribute="", name=""):
     """
     if str(type(obj)) != "<class '__main__.MyClass'>":
         raise TypeError("can't add new attribute")
-    if attribute == "" or name == "":
+    if attribute == "" or name == "" or obj == "":
         raise TypeError("can't add new attribute")
     if type(attribute) is not str or type(name) is not str:
         raise TypeError("can't add new attribute")
