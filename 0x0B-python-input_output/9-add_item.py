fd01610abb73cdc15d6e66a149d4d09d35b47409
erikaosgue <1476@holbertonschool.com>
2020-06-03 23:56:53 +0000

    first version

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    9. Load, add, save
+    module: 9-add_item.py
+"""
+import sys
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+
+
+filename = "add_item.json"
+lista = []
+
+try:
+    lista = load_from_json_file(filename)
+except FileNotFoundError:
+    lista = []
+
+lista.extend(sys.argv[1:])
+save_to_json_file(lista, filename)
