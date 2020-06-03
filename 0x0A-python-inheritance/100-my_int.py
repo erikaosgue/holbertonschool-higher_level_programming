d99165cfca3935d48e8f045eb1feb44347c1a536
erikaosgue <1476@holbertonschool.com>
2020-06-03 00:16:14 +0000

    first version

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""12. My integer"""
+
+
+class MyInt(int):
+    """ a class MyInt that inherits from int"""
+    def __init__(self, number):
+        self.number = number
+
+    def __eq__(self, other):
+        return not (self.number == other)
+    def __ne__(self, other):
+        return not (self.number != other)
