9681c40259c5045ae473b6a444815928de9362db
erikaosgue <1476@holbertonschool.com>
2020-06-03 00:53:56 +0000

    Add import modules

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -4,6 +4,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+
     def __init__(self, size):
         super().integer_validator("size", size)
         self.__size = size
