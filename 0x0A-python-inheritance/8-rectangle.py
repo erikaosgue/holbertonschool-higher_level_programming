6081d00f2d68cbf612471d8381a7ad6baedf4395
erikaosgue <1476@holbertonschool.com>
2020-06-03 00:49:45 +0000

    Add import modules

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """ class Rectangle that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
         super().integer_validator("width", width)
