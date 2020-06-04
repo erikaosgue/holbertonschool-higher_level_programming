a0995f3733f2b66877397b1dcf23ea019f605a77
erikaosgue <1476@holbertonschool.com>
2020-06-04 04:40:17 +0000

    Add Advanced task

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    15. Search and update
+    module: 100-append_after.py
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """append_after: inserts a line of text to a file, after each line
+    containing a specific string
+
+    Args:
+        filename (str): name of the file
+        search_string (str) string to find in the file
+        new_string (str, optional): string to at after
+        finding the search string
+    """
+    with open(filename, 'r+') as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith(search_string) or search_string in line:
+                lines[i] = lines[i].strip() + '\n' + new_string
+        f.seek(0)
+        for line in lines:
+            f.write(line)
