d73fb15c86da69d7993816c36fedefa981c02f65
erikaosgue <1476@holbertonschool.com>
2020-06-04 01:18:53 +0000

    Add first version

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-    11. Student to JSON
-    module: 11-student.py
+    12. Student to JSON with filter
+    module: 12-student.py
 """
 
 
@@ -19,7 +19,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if (attrs is not None) and type(attrs) is list and\
+        if (attrs) and type(attrs) is list and\
                 all(type((item) is str) for item in attrs):
             dic = {}
             for key in attrs:
