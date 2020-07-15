a282a26dd3e68d5d69eae28d8de88c0c7bd4b962
erikaosgue <1476@holbertonschool.com>
2020-07-15 04:51:44 +0000

    Add task 20: Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+--  Best genre
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT name, sum(rate) AS rating
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
+GROUP BY name
+ORDER BY rating DESC;
