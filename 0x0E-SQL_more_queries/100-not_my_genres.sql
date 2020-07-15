882b7a7d7fefd0e14830acf3914af772e2974b66
erikaosgue <1476@holbertonschool.com>
2020-07-15 03:54:51 +0000

    Add task 17: Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,4 +7,5 @@ WHERE name NOT IN (
     FROM tv_shows ts 
     JOIN tv_show_genres tsg ON ts.id = tsg.show_id
     JOIN tv_genres tg ON tsg.genre_id = tg.id
-    WHERE ts.title = 'Dexter');
+    WHERE ts.title = 'Dexter')
+ORDER BY name;
