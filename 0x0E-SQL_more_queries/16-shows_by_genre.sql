5caeeb55f6bf199921e0e6bcdc5f8a9638f783ab
erikaosgue <1476@holbertonschool.com>
2020-07-15 02:10:05 +0000

    Add task 16: lists all shows, and all genres linked to that show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,5 +3,5 @@
 SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-left JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name; 
