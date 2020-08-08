4f32cc24af8445ec7fd26b3886fb87d39f7869a3
erikaosgue <1476@holbertonschool.com>
2020-08-08 19:18:47 +0000

    Add task 1: Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" A script that lists all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa:"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ main function """
+    if len(argv) == 4:
+        my_user = argv[1]
+        my_passw = argv[2]
+        my_db = argv[3]
+        try:
+            db = MySQLdb.connect(host="localhost", user=my_user,
+                                 passwd=my_passw, db=my_db, port=3306)
+        except:
+            print("[Error]: one or more argms invalid")
+            return 0
+
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM states WHERE states.name LIKE 'N%'
+                       ORDER BY states.id""")
+        cursor.fetchall()
+        for row in cursor:
+            print(row)
+        db.close()
+
+
+if __name__ == "__main__":
+    main()
