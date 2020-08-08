a7d7fdaf79e54bcff09c1e168e2d8563e46349dd
erikaosgue <1476@holbertonschool.com>
2020-08-08 19:19:36 +0000

    Add task 3: SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" a script that takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument. """
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ main function """
+    if len(argv) == 5:
+        my_user = argv[1]
+        my_passw = argv[2]
+        my_db = argv[3]
+        name = argv[4]
+        try:
+            db = MySQLdb.connect(host="localhost", user=my_user,
+                                 passwd=my_passw, db=my_db, port=3306)
+        except:
+            print("[Error]: one or more argms invalid")
+            return 0
+
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM states WHERE states.name in (%s)
+                       ORDER BY states.id""", (name,))
+        cursor.fetchall()
+        for row in cursor:
+            print(row)
+        db.close()
+
+
+if __name__ == "__main__":
+    main()
