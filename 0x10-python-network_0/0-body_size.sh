450d3998766373bef0f3b3e756ccb11fc37361c8
erikaosgue <1476@holbertonschool.com>
2020-08-18 20:33:08 +0000

    Update task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,7 +2,5 @@
 # a Bash script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
 # s silent option, quiet mode, so don't show more than what i am asking for
-# I  Fetch the headers only!
-
-curl -s "$1"| wc -c 
-
+# wc -c count the number of bytes
+curl -s "$1" | wc -c
