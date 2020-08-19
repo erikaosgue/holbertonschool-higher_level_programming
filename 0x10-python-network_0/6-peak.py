7585dbcdf60cb26b68d7e0909ee95b10bfe299f3
erikaosgue <1476@holbertonschool.com>
2020-08-19 02:15:59 +0000

    Add task 6:  Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,10 +2,19 @@
 """ Find a peak """
 
 
+def my_max(list_of_integers):
+    """Find the max number of the list"""
+    max_value = list_of_integers[0]
+    for i in list_of_integers:
+        if max_value < i:
+            max_value = i
+    return max_value
+
+
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers."""
     if isinstance(list_of_integers, list) and list_of_integers:
-        max_value = max(list_of_integers)
+        max_value = my_max(list_of_integers)
         return (max_value)
     else:
         return None
