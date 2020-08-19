b5df0451f7c2ae4d48f69f54a37a909a2e84427b
erikaosgue <1476@holbertonschool.com>
2020-08-19 03:38:35 +0000

    Update task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,19 +2,23 @@
 """ Find a peak """
 
 
-def my_max(list_of_integers):
-    """Find the max number of the list"""
-    max_value = list_of_integers[0]
-    for i in list_of_integers:
-        if max_value < i:
-            max_value = i
-    return max_value
-
-
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers."""
-    if isinstance(list_of_integers, list) and list_of_integers:
-        max_value = my_max(list_of_integers)
-        return (max_value)
-    else:
+    """finds a max_value in a list of unsorted integers."""
+    if not isinstance(list_of_integers, list) or list_of_integers == []:
         return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        else:
+            return list_of_integers[1]
+    middle = int(len(list_of_integers) / 2)
+    max_value = list_of_integers[middle]
+    if max_value > list_of_integers[middle - 1] and \
+            max_value > list_of_integers[middle + 1]:
+        return max_value
+    elif max_value < list_of_integers[middle - 1]:
+        return find_peak(list_of_integers[:middle])
+    else:
+        return find_peak(list_of_integers[middle + 1:])
