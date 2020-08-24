12f2c2e4d473ed2ffb93db1ea7497526de44cfa5
erikaosgue <1476@holbertonschool.com>
2020-08-24 19:25:15 +0000

    Add task 4: What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Script that fetches https://intranet.hbtn.io/status using package requests
+Usage: ./4-hbtn_status.py """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    body = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(body.text)))
+    print("\t- content: {}".format(body.text))
