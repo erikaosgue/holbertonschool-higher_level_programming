6dbb022f77a9053773862b6fcfded7b4059db641
erikaosgue <1476@holbertonschool.com>
2020-08-24 19:08:31 +0000

    Add task 2: POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     values = {"email": sys.argv[2]}
     # encoding the data (email) into bytes
-    data = urllib.parse.urlencode(values)encode(" utf-8")
+    data = urllib.parse.urlencode(values).encode(" utf-8")
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as response:
