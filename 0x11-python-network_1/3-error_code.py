ba2cb259615facad69fc7d9385863fb3751b934a
erikaosgue <1476@holbertonschool.com>
2020-08-29 01:38:20 +0000

    Update task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Takes in a URL, sends a request to the URL and displays the body of the
 response (decoded in utf-8).
-Usage: ./3-error_code.py http://0.0.0.0:5000 """
+Usage: ./3-error_code.py http://0.0.0.0:5000
+"""
 
 
 import urllib.request
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     try:
-        response = urllib.request.urlopen(url)
-        print(response.read().decode("utf-8"))
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
