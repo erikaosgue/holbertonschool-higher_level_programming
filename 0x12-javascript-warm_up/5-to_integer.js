1628dae38a5cfea83451c9d6ef268fd3646879d6
erikaosgue <1476@holbertonschool.com>
2020-09-15 23:41:19 +0000

    Add task 5: An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-
-if (process.argv.length < 3) {
-  console.log('Not a number');
-} else {
-  const result = parseInt(process.argv[2]);
-  if (result === result) {
-    console.log('My number:', result);
-  } else {
-    console.log('Not a number');
-  }
-}
+const result = parseInt(Number(process.argv[2]));
+console.log(isNaN(result) ? 'Not a number' : `My number: ${result}`);
