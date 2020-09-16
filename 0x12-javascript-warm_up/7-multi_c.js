b5a80286ddfa8cce1e02aeae91503d58b9209edd
erikaosgue <1476@holbertonschool.com>
2020-09-16 04:13:16 +0000

    Hotfix task: 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const result = parseInt(Number(process.argv[2]));
-if (isNaN(result)){
-    console.log('Missing number of occurrences')
-}
-else {
+if (isNaN(result)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < result; i++) {
     console.log('C is fun');
   }
